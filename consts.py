0cd97690500a6657ba93e99062a008f449510c91
Victor Borneo <victorbor2001@yahoo.com.br>
2026-02-02 18:47:00 -0300

    more rgb

--- consts.py
+++ consts.py
@@ -4,9 +4,12 @@ GAME_ICON = "assets/icon.png"
 # RGB
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+DARK_ORANGE = (255, 140, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
