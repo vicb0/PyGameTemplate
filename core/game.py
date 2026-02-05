4366ad37222701ab420b3881fe8499cdb43b718d
Victor Borneo <victorbor2001@yahoo.com.br>
2026-02-05 14:44:37 -0300

    fixed icon path not using resource_path

--- core/game.py
+++ core/game.py
@@ -6,10 +6,11 @@ from managers.AudioManager import AudioManager
 from managers.ScreensManager import ScreensManager
 from managers.InputsManager import InputsManager
 from managers.SettingsManager import SettingsManager
+from utils.resourceUtils import resource_path
 
 
 SCREEN_TITLE = getattr(metadata, "SCREEN_TITLE", "Pygame")
-GAME_ICON_PATH = getattr(metadata, "GAME_ICON_PATH", "assets/icon.png")
+GAME_ICON_PATH = resource_path(getattr(metadata, "GAME_ICON_PATH", "assets/icon.png"))
 DEFAULT_SCREEN = getattr(metadata, "DEFAULT_SCREEN", "mainmenu")
 
 
